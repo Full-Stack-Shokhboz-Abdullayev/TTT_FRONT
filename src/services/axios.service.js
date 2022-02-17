03481c216c29846832d8e1f6bbc77bc5c78f0dc1
Shokhboz Abdullaev <shokhboz_abdullaev@epam.com>
2022-02-17 12:25:44 +0500

    fix: TTT_FRONT: api base url

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
 
 const axiosService = axios.create({
-  baseURL: BASE_URL,
+  baseURL: API_BASE_URL,
 });
 
 axiosService.interceptors.request.use((config) => {
